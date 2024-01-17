942bc6cabfdb511a09ea3f474d5aa2fdafbf1b7b
Leonardo Rodrigues de Mello <lrodriguesdemello@zendesk.com>
2024-01-17 17:05:06 -0500

    Initial contribution work in progress

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # opencost-parquet-exporter
 Export OpenCost data in parquet format
+
+This script was created to export data from opencost in PARQUET format.
+
+It supports exporting the data to S3 and local directory.
+
+# Dependencies
+This script depends on boto3, pandas, numpy and python-dateutil.
+
+The file requirements.txt has all the dependencies specified.
+
+# Configuration:
+The script supports the following environment variables:
+* OPENCOST_PARQUET_SVC_HOSTNAME: Hostname of the opencost service. By default it assume the opencost service is on localhost.
+* OPENCOST_PARQUET_SVC_PORT: Port of the opencost service, by default it assume it is 9003
+* OPENCOST_PARQUET_WINDOW_START: Start window for the export, by default it is None, which results in exporting the data for yesterday.
+* OPENCOST_PARQUET_WINDOW_END: End of export window, by default it is None, which results in exporting the data for yesterday.
+* OPENCOST_PARQUET_S3_BUCKET: S3 bucket that will be used to store the export. By default this is None, and S3 export is not done. If set to a bucket use s3://bucket-name and make sure there is an AWS Role  with access to the s3 bucket attached to the container that is running the export. This also respect the environment variables AWS_PROFILE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY. see: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+* OPENCOST_PARQUET_FILE_KEY_PREFIX: This is the prefix used for the export, by default it is '/tmp'. The export is going to be saved inside this prefix, in the following structure: year=window_start.year/month=window_start.month/day=window_start.day , ex: tmp/year=2024/month=1/date=15
+* OPENCOST_PARQUET_AGGREGATE: This is the dimentions used to aggregate the data. by default we use "namespace,pod,container" which is the same dimensions used for the CSV native export.
+* OPENCOST_PARQUET_STEP: This is the Step for the export, by default we use 1h steps, which result in 24 steps in a day and make easier to match the exported data to AWS CUR, since cur also export on hourly base.
+
+# Usage:
+
+## Export yesterday data
+```
+# 1. Create virtualenv and install dependencies
+$python3 -m venv .venv
+$pip install requirements.txt
+# 2. Configuration
+## 2.1 Set any desired variables
+$export OPENCOST_PARQUET_FILE_KEY_PREFIX="/tmp/cluster=YOUR_CLUSTER_NAME"
+## 2.2 Make sure the script has access to opencost api.
+### if running from your laptop for testing, create a port forward to opencost
+$kubectl -n opencost port-forward service/opencost 9003:9003
+
+# 3. Run the script to export data
+$python3 opencost_parquet_exporter.py
+# 4. Check the exported parquet files
+$ls /tmp/cluster=YOUR_CLUSTER_NAME
+```
+
+## Backfill old data.
+
+You can only backfill data that is still available in the opencost API.
+
+To backfill old data you need to set both the OPENCOST_PARQUET_WINDOW_START AND OPENCOST_PARQUET_WINDOW_END for each day you want to backfill, then run the script one time for each day.
+
+Another option is to use something like the code bellow, to backfill multiple days at once.:
+
+```
+import opencost_parquet_exporter
+for days in range(1,18):
+     config = opencost_parquet_exporter.get_config(window_start=f"2024-01-{str(days).zfill(2)}T00:00:00Z", window_end=f"2024-01-{str(days).zfill(2)}T23:59:59Z")
+     print(config)
+     result = opencost_parquet_exporter.request_data(config)
+     processed_data = opencost_parquet_exporter.process_result(result,config)
+     opencost_parquet_exporter.save_result(processed_data, config)
+```
+
+# Recommended setup:
+Run this script as a k8s cron job once per day.
+
+If you run on multiple clusters, set the OPENCOST_PARQUET_FILE_KEY_PREFIX with a unique indentifier per cluster.
+
+# TODO
+
+The following tasks need to be completed:
+* Add testing to the code
+* Create a docker image
+* Configure the automated build for the docker image
+* Create example k8s cron job resource configuration
+* Create Helm chart
+* Improve script to set data_types using a k8s configmap
+* Improve script to set the rename_columns_config using a k8s config map
+
+We are looking forward to have your contributions.
---
+++ src/opencost_parquet_exporter.py
@@ -0,0 +1,169 @@
+import requests
+import json
+import os
+from datetime import datetime, timedelta
+import pandas as pd
+import boto3
+
+
+def get_config(
+        hostname=os.environ.get('OPENCOST_PARQUET_SVC_HOSTNAME', 'localhost'),
+        port=os.environ.get('OPENCOST_PARQUET_SVC_PORT', 9003),
+        window_start=os.environ.get('OPENCOST_PARQUET_WINDOW_START', None),
+        window_end=os.environ.get('OPENCOST_PARQUET_WINDOW_END', None),
+        s3_bucket=os.environ.get('OPENCOST_PARQUET_S3_BUCKET', None),
+        file_key_prefix=os.environ.get('OPENCOST_PARQUET_FILE_KEY_PREFIX',
+                                       '/tmp/'),
+        aggregate_by=os.environ.get('OPENCOST_PARQUET_AGGREGATE',
+                                    'namespace,pod,container'),
+        step=os.environ.get('OPENCOST_PARQUET_STEP', '1h')):
+    config = {}
+    config['url'] = "http://{}:{}/allocation/compute".format(hostname, port)
+    config['file_key_prefix'] = file_key_prefix
+    # If window is not specified assume we want yesterday data.
+    if window_start is None or window_end is None:
+        yesterday = datetime.strftime(
+            datetime.now() - timedelta(1), '%Y-%m-%d')
+        window_start = yesterday+'T00:00:00Z'
+        window_end = yesterday+'T23:59:59Z'
+    window = '{},{}'.format(window_start, window_end)
+    config['params'] = (
+        ("window", window),
+        ("aggregate", aggregate_by),
+        ("includeIdle", "false"),
+        ("idleByNode", "false"),
+        ("includeProportionalAssetResourceCosts", "false"),
+        ("format", "json"),
+        ("step", step)
+    )
+    # This is required to ensure consistency without this
+    # we could have type change from int to float over time
+    # And this will result in an HIVE PARTITION SCHEMA MISMATCH
+    config['data_types'] = {
+        'cpuCoreHours': 'float',
+        'cpuCoreRequestAverage': 'float',
+        'cpuCoreUsageAverage': 'float',
+        'cpuCores': 'float',
+        'cpuCost': 'float',
+        'cpuCostAdjustment': 'float',
+        'cpuEfficiency': 'float',
+        'externalCost': 'float',
+        'gpuCost': 'float',
+        'gpuCostAdjustment': 'float',
+        'gpuCount': 'float',
+        'gpuHours': 'float',
+        'loadBalancerCost': 'float',
+        'loadBalancerCostAdjustment': 'float',
+        'networkCost': 'float',
+        'networkCostAdjustment': 'float',
+        'networkCrossRegionCost': 'float',
+        'networkCrossZoneCost': 'float',
+        'networkInternetCost': 'float',
+        'networkReceiveBytes': 'float',
+        'networkTransferBytes': 'float',
+        'pvByteHours': 'float',
+        'pvBytes': 'float',
+        'pvCost': 'float',
+        'pvCostAdjustment': 'float',
+        'ramByteHours': 'float',
+        'ramByteRequestAverage': 'float',
+        'ramByteUsageAverage': 'float',
+        'ramBytes': 'float',
+        'ramCost': 'float',
+        'ramCostAdjustment': 'float',
+        'ramEfficiency': 'float',
+        'running_minutes': 'float',
+        'sharedCost': 'float',
+        'totalCost': 'float',
+        'totalEfficiency': 'float'
+    }
+    config['ignored_alloc_keys'] = ['pvs', 'lbAllocations']
+    config['rename_columns_config'] = {
+        'start': 'running_start_time',
+        'end': 'running_end_time',
+        'minutes': 'running_minutes',
+        'properties.labels.node_type': 'label.node_type',
+        'properties.labels.product': 'label.product',
+        'properties.labels.project': 'label.project',
+        'properties.labels.role': 'label.role',
+        'properties.labels.team': 'label.team',
+        'properties.namespaceLabels.product': 'namespaceLabels.product',
+        'properties.namespaceLabels.project': 'namespaceLabels.project',
+        'properties.namespaceLabels.role': 'namespaceLabels.role',
+        'properties.namespaceLabels.team': 'namespaceLabels.team'
+    }
+    config['window_start'] = window_start
+    return config
+
+
+def request_data(config):
+    url, params = config['url'], config['params']
+    try:
+        response = requests.get(
+            url,
+            params=params,
+        )
+        response.raise_for_status()
+        if 'application/json' in response.headers['content-type']:
+            response_object = response.json()['data']
+            return response_object
+        else:
+            print(f"Invalid content type: {response.headers['content-type']}")
+            return None
+    except (requests.exceptions.RequestException, requests.exceptions.Timeout,
+            requests.exceptions.TooManyRedirects, ValueError) as err:
+        print(f"Request error: {err}")
+        return None
+
+
+def process_result(result, config):
+    for split in result:
+        # Remove entry for unmounted pv's .
+        # this break the table schema in athena
+        split.pop('__unmounted__/__unmounted__/__unmounted__', None)
+    for split in result:
+        for alloc_name in split.keys():
+            for ignored_key in config['ignored_alloc_keys']:
+                split[alloc_name].pop(ignored_key, None)
+    try:
+        frames = [pd.json_normalize(split.values()) for split in result]
+        processed_data = pd.concat(frames)
+        processed_data.rename(
+            columns=config['rename_columns_config'], inplace=True)
+        processed_data = processed_data.astype(config['data_types'])
+    except Exception as err:
+        print(f"Error during panda dataframe processing {str(err)}")
+        return None
+    return processed_data
+
+
+def save_result(processed_result, config):
+    file_name = 'k8s_opencost.parquet'
+    window = datetime.strptime(config['window_start'], "%Y-%m-%dT%H:%M:%SZ")
+    parquet_prefix = '{}/year={}/month={}/day={}'.format(
+        config['file_key_prefix'], window.year, window.month, window.day)
+    try:
+        if config.get('s3_bucket', None):
+            uri = 's3://{}/{}/{}'.format(config['s3_bucket'],
+                                         parquet_prefix, filename)
+        else:
+            uri = 'file://{}/{}'.format(parquet_prefix, file_name)
+            path = '/'+parquet_prefix
+            os.makedirs(path, 0o750, exist_ok=True)
+        processed_result.to_parquet(uri)
+    except Exception as err:
+        print(f"Error {str(err)}")
+
+
+def main():
+    try:
+        config = get_config()
+        result = request_data(config)
+        processed_data = process_result(result, config)
+        save_result(processed_data, config)
+    except Exception as err:
+        print(f"Unexpected error {str(err)}")
+
+
+if __name__ == "__main__":
+    main()
